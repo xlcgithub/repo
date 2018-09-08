4793eefc3b290d64ac41e7e959fde538f9cd01e4
xlc <chenxiaoli101603@163.com>
2018-09-08 16:54:33 +0800

    添加test 测试方法

--- git_test/src/com/xlc/GitTest.java
+++ git_test/src/com/xlc/GitTest.java
@@ -4,4 +4,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("111111111");
     }
+    public void test(){
+        System.out.println("22222222");
+    }
 }
